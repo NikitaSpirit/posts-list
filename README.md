5f5cae21da52021bcf1e655aa28ce98d501fe84b
Nikita Ustinov <Nikita.Ustinov@ventionteams.com>
2023-09-26 15:06:02 +0300

    Update readme

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ View and filter the list of all posts!
 1. View the list of all posts.
 2. Filter displayed posts by user id.
 3. Expand post to see the comments.
+
+## To start the application
+Run `npm run start` inside the root directory.
--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -1,7 +1,6 @@
 import { AxiosError } from "axios";
 import { IPost } from "../api/getPosts";
 import { IUser } from "../api/getUsers";
-import  { IComment } from "../api/getCommentsByPostId";
 
 export const FETCH_POSTS_REQUEST = "FETCH_POSTS_REQUEST";
 export const FETCH_POSTS_SUCCESS = "FETCH_POSTS_SUCCESS";
@@ -10,10 +9,6 @@ export const FETCH_POSTS_FAILURE = "FETCH_POSTS_FAILURE";
 export const FETCH_USERS_REQUEST = "FETCH_USERS_REQUEST";
 export const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS";
 export const FETCH_USERS_FAILURE = "FETCH_USERS_FAILURE";
-
-export const FETCH_POST_COMMENTS_REQUEST = "FETCH_POST_COMMENTS_REQUEST";
-export const FETCH_POST_COMMENTS_SUCCESS = "FETCH_POST_COMMENTS_SUCCESS";
-export const FETCH_POST_COMMENTS_FAILURE = "FETCH_POST_COMMENTS_FAILURE";
 
 export const fetchPostsRequest = () => ({
   type: FETCH_POSTS_REQUEST,
@@ -38,15 +33,3 @@ export const fetchUsersFailure = (error: AxiosError) => ({
   type: FETCH_USERS_FAILURE,
   payload: error,
 });
-
-// export const fetchPostCommentsRequest = () => ({
-//   type: FETCH_POST_COMMENTS_REQUEST,
-// });
-// export const fetchPostCommentsSuccess = (users: IComment[]) => ({
-//   type: FETCH_POST_COMMENTS_SUCCESS,
-//   payload: users,
-// });
-// export const fetchPostCommentsFailure = (error: AxiosError) => ({
-//   type: FETCH_POST_COMMENTS_FAILURE,
-//   payload: error,
-// });
